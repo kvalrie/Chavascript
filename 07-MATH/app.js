0c6d24b6ee2dfddeec90e1c4e47aa06482e3b5be
kvalrie <hugo.barcelona31@gmail.com>
2017-06-08 16:32:49 +0200

    + 07

---
+++ 07-MATH/app.js
@@ -0,0 +1,31 @@
+var min = Math.min( 7, 5, -12, 6, 32, 18, 14, -2);
+var max = Math.max(-3, 9, 21, 36, 27, 54, 17, 35);
+var add = min + max
+
+console.log(min)
+console.log(max)
+console.log(add)
+
+var floatBateau = 10.4;
+var couleBateau = Math.floor(floatBateau);
+var voleBateau = Math.ceil(floatBateau);
+
+console.log(floatBateau);
+console.log(couleBateau);
+console.log(voleBateau);
+
+var ale50100 = (Math.random()*(50))+ 50;
+var ale01=Math.round(Math.random());
+var ale010= (Math.random() * (10));
+
+console.log(ale50100);
+console.log(ale01);
+console.log(ale010);
+
+
+
+
+/*
+La valeur aléatoire entre 50 et 100
+La valeur aléatoire peut être seulement 0 ou 1
+La valeur aléatoire peut aller de 0 à 10 inclu*/
